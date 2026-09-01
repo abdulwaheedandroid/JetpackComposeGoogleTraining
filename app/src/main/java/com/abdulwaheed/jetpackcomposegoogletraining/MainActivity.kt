72f66496d69a41dbc398d0701f4b3ed2a6861646
abdulwaheed-cp <abdul.waheed@cloudprimero.com>
2026-09-01 05:30:21 +0500

    Added image composable component

--- app/src/main/java/com/abdulwaheed/jetpackcomposegoogletraining/MainActivity.kt
+++ app/src/main/java/com/abdulwaheed/jetpackcomposegoogletraining/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,14 +30,21 @@ class MainActivity : ComponentActivity() {
             JetPackComposeGoogleTrainingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     GreetingText(
-                        message = "Happy Birthday Sam!",
-                        from = "From Emma",
+                        message = stringResource(R.string.happy_birthday_sam),
+                        from = stringResource(R.string.signature_text),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_launcher_background)
+    Image(image, contentDescription = null)
+
 }
 
 @Composable
