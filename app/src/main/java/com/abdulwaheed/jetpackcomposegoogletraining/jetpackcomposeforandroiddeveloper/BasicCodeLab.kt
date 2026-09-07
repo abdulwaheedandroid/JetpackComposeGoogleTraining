ed483f646fd9a80601c57265359c00b0a3717ac9
abdulwaheed-cp <abdul.waheed@cloudprimero.com>
2026-09-08 04:17:14 +0500

    in this commit i have learned about modifieres and rows, colmns and remember keyword

--- app/src/main/java/com/abdulwaheed/jetpackcomposegoogletraining/jetpackcomposeforandroiddeveloper/BasicCodeLab.kt
+++ app/src/main/java/com/abdulwaheed/jetpackcomposegoogletraining/jetpackcomposeforandroiddeveloper/BasicCodeLab.kt
@@ -1,19 +1,27 @@
 package com.abdulwaheed.jetpackcomposegoogletraining.jetpackcomposeforandroiddeveloper
 
+import android.R.attr.name
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.abdulwaheed.jetpackcomposegoogletraining.ui.theme.JetPackComposeGoogleTrainingTheme
+import java.nio.file.WatchEvent
 
 class BasicCodeLab : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +30,14 @@ class BasicCodeLab : ComponentActivity() {
         setContent {
             JetPackComposeGoogleTrainingTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    Greeting(name = "Android")
+                    Row {
+                        Column(modifier = Modifier.padding(vertical = 4.dp)) {
+                            val names: List<String> = mutableListOf("World", "Compose")
+                            for (name in names) {
+                                Greeting(name = name)
+                            }
+                        }
+                    }
                 }
 
             }
@@ -32,7 +47,25 @@ class BasicCodeLab : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    val expanded = remember {mutableStateOf(false)}
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row (modifier = Modifier.padding(24.dp)){
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+
+            ) {
+                Text("Hello, $name")
+            }
+
+            OutlinedButton(onClick = {expanded.value = !expanded.value}) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
     }
 }
